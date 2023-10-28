e9dc730f0a33bfb3f4f0fcd770e0e61a4d12e2fc
PavelShpagin <pavelandrewshpagin@gmail.com>
2023-10-28 17:02:07 +0300

    updated filepath handling

--- Lab2/RootDir.cs
+++ Lab2/RootDir.cs
@@ -10,18 +10,13 @@ namespace Lab2
     {
 		public static string Get(string path)
 		{
-			// Get the base directory of the current application domain
 			string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-
-			// Navigate up the directory structure until you find a file with the ".sln" extension,
-			// which typically indicates the root of the project
 			string directory = baseDirectory;
 			while (!Directory.GetFiles(directory, "*.sln").Any())
 			{
 				string parentDirectory = Directory.GetParent(directory).FullName;
 				if (parentDirectory == directory)
 				{
-					// Reached the root without finding a solution file
 					break;
 				}
 				directory = parentDirectory;
